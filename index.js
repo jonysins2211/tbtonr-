102a711a7c1d4814caa358c7f4b4b6d8467b2211
jonysins2211 <Jonysins11220@gmail.com>
2026-02-26 22:00:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ bot.on('callback_query', async (ctx) => {
 
 // 🌐 Tamilmv Scraper Function
 async function tamilmv() {
-    const url = 'https://www.1tamilmv.gold/';
+    const url = 'https://www.1tamilmv.gs/';
     const headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
     };
